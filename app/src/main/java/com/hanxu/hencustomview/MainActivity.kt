40ab67040635e64c13fc0a3983dde8863dc35b95
hanxu <2327727406@qq.com>
2021-05-28 18:24:05 +0800

    属性动画

--- app/src/main/java/com/hanxu/hencustomview/MainActivity.kt
+++ app/src/main/java/com/hanxu/hencustomview/MainActivity.kt
@@ -2,8 +2,11 @@ package com.hanxu.hencustomview
 
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
+import android.animation.TypeEvaluator
+import android.graphics.PointF
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.hanxu.hencustomview.view.ProvinceEvaluator
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +14,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        cameraViewAnimation()
+//        cameraViewAnimation()
+//        pointFAnimation()
+        provinceAnimation()
     }
 
 //    fun circleViewAnimation(){
@@ -39,5 +44,32 @@ class MainActivity : AppCompatActivity() {
         val animatorSet = AnimatorSet()
         animatorSet.playSequentially(topFlipAnimator,bottomFlipAnimator, flipRotationAnimator )
         animatorSet.start()
+    }
+
+    private fun pointFAnimation(){
+        val animator = ObjectAnimator.ofObject(view, "point", PointFEvaluator(), PointF(200.dp, 300.dp))
+        animator.startDelay = 1000
+        animator.duration = 2000
+        animator.start()
+    }
+
+    private fun provinceAnimation(){
+        val animator = ObjectAnimator.ofObject(view, "province", ProvinceEvaluator(), "澳门特别行政区")
+        animator.startDelay = 500
+        animator.duration = 10000
+        animator.start()
+
+    }
+
+    class PointFEvaluator : TypeEvaluator<PointF> {
+        override fun evaluate(fraction: Float, startValue: PointF, endValue: PointF): PointF {
+            val startX = startValue.x
+            val endX = endValue.x
+            val currentX = startX + (endX - startX) * fraction
+            val startY = startValue.y
+            val endY = endValue.y
+            val currentY = startY + (endY - startY) * fraction
+            return PointF(currentX, currentY)
+        }
     }
 }
